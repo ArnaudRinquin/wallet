d9e34aeb46b0e16cc2389e17168c4a82a8ef2019
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2014-07-29 17:17:07 +0200

    Add heroku compliant static server

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(__dirname + '/build'));
+app.listen(port);
+
+module.exports = app;
