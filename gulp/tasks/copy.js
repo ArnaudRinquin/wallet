39cbdfb6c6aceadf86d9abdd263a9f72d83a8ed2
Arnaud Rinquin <rinquin.arnaud@gmail.com>
2014-07-31 01:11:08 +0200

    Add  integration

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,11 +1,19 @@
 var gulp = require('gulp');
 
-gulp.task('copy', function() {
+gulp.task('copy', ['copyFonts'], function() {
 	return gulp.src([
     'bower_components/bootstrap/dist/css/bootstrap.min.css',
     'bower_components/bootstrap/dist/css/bootstrap-theme.min.css',
     'bower_components/bootstrap-switch/dist/css/bootstrap3/bootstrap-switch.min.css',
+    'bower_components/font-awesome/css/font-awesome.min.css',
     'src/htdocs/**'
     ])
 		.pipe(gulp.dest('build'));
 });
+
+gulp.task('copyFonts', function(){
+  return gulp.src([
+    'bower_components/font-awesome/fonts/**',
+    ])
+    .pipe(gulp.dest('build/fonts'));
+})
